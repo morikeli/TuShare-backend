642cc6ec522908fe3c8a57dac5faeaeb8a4e0fc7
magnus.coder <kelimbth@gmail.com>
2025-02-18 17:52:13 +0300

    mount media files

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from database import Base, engine
 from auth import router as auth_router
 from users import router as users_router
 from rides import router as rides_router
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.mount("/media/dps", StaticFiles(directory="media/dps"), name="uploads")
 
 app.include_router(auth_router)
 app.include_router(users_router)
