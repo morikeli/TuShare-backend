73de599cc4cb2efed3c2cb344b79663e8ff79776
magnus.coder <kelimbth@gmail.com>
2025-02-18 18:05:22 +0300

    updated endpoint for editing user profile

--- users.py
+++ users.py
@@ -44,10 +44,15 @@ def create_user(username: str, email: str, password: str, profile_image: UploadF
 
 @router.put("/profile/edit/")
 def edit_profile(image: UploadFile = File(...), db: Session = Depends(get_db), current_user: User = Depends(get_db)):
-    file_location = f"./images/{image.filename}"
+    file_location = f"./media/dps/{image.filename}"
     with open(file_location, "wb") as file_object:
         file_object.write(image.file.read())
     current_user.profile_image = file_location
     db.commit()
     db.refresh(current_user)
-    return {"info": f"Profile image uploaded at {file_location}"}
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
+        "profile_image": f"/media/dps/{os.path.basename(file_location)}" if file_location else None
+    }
