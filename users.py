2fe16819b9f00ac3f23c1ace30060eea65c3262f
magnus.coder <kelimbth@gmail.com>
2025-02-20 17:12:29 +0300

    updated profile endpoint

--- users.py
+++ users.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, Depends, UploadFile, File
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.database import get_db
+from db.schemas import UpdateUserProfile
 from models import User
+from utils import get_current_user
 import aiofiles
 import os
+
 
 router = APIRouter()
 
 
 @router.put("/profile/edit/")
-async def edit_profile(image: UploadFile = File(...), db: AsyncSession = Depends(get_db), current_user: User = Depends(get_db)):
+async def edit_profile(profile_data: UpdateUserProfile, image: UploadFile = File(...), db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
     file_location = f"./media/dps/{image.filename}"
     
     # Save the uploaded image asynchronously
@@ -17,7 +20,13 @@ async def edit_profile(image: UploadFile = File(...), db: AsyncSession = Depends
         while chunk := await image.read(1024):
             await file_object.write(chunk)
     
+    # update user profile fields
     current_user.profile_image = file_location
+
+    # update user fields dynamically
+    for field, value in profile_data.model_dump().items():
+        setattr(current_user, field, value if value is not None else getattr(current_user, field))
+
     await db.commit()
     await db.refresh(current_user)
     
@@ -25,5 +34,11 @@ async def edit_profile(image: UploadFile = File(...), db: AsyncSession = Depends
         "id": current_user.id,
         "username": current_user.username,
         "email": current_user.email,
-        "profile_image": f"/media/dps/{os.path.basename(file_location)}" if file_location else None
+        "profile_image": f"/media/dps/{os.path.basename(file_location)}" if file_location else None,
+         "facebook_handle": current_user.facebook_handle,
+        "instagram_handle": current_user.instagram_handle,
+        "twitter_handle": current_user.twitter_handle,
+        "work_address": current_user.work_address,
+        "home_address": current_user.home_address,
+        "bio": current_user.bio
     }
