116a7aa9e7167cf5712ccf63dfb4660b50031f69
magnus.coder <kelimbth@gmail.com>
2025-02-20 16:08:05 +0300

    created new functions to initializa and get database

--- db/database.py
+++ db/database.py
@@ -9,3 +9,14 @@ engine = create_async_engine(DATABASE_URL, connect_args={"check_same_thread": Fa
 AsyncSessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+
+    async with AsyncSessionLocal() as db:
+        yield db
+        await db.close()
