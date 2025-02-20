7008cb36a15a9ee5bbd638828583282aa18302d5
magnus.coder <kelimbth@gmail.com>
2025-02-20 16:07:15 +0300

    deleted unused module

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from typing import AsyncGenerator
 
 
