5c2ae4a12f63aebe70749ad32fd78a984652962e
magnus.coder <kelimbth@gmail.com>
2025-02-20 14:24:22 +0300

    replaced deprecated datetime.utcnow() with datetime.now(timezone.utc)

--- utils.py
+++ utils.py
@@ -22,9 +22,9 @@ def verify_password(plain_password, hashed_password):
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
