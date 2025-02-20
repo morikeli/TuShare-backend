3d38e78929107f1f3230ff962eadb9bb1b986935
magnus.coder <kelimbth@gmail.com>
2025-02-20 14:31:26 +0300

    updated names to match attributes used in the User model

--- auth.py
+++ auth.py
@@ -33,7 +33,7 @@ def get_db():
 def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(User).filter(User.username == form_data.username).first()
 
-    if not user or not verify_password(form_data.password, user.hashed_password):
+    if not user or not verify_password(form_data.password, user.password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
 
     # Update the last_login field
@@ -75,8 +75,8 @@ def create_user(user: UserCreate = Depends(), profile_image: UploadFile = File(N
     hashed_password = get_password_hash(user.password)
     
     user_data = user.model_dump()
-    user_data['profile_picture'] = image_path
-    user_data['hashed_password'] = hashed_password
+    user_data['profile_image'] = image_path
+    user_data['password'] = hashed_password
 
     db_user = User(**user_data)
 
