84e2296562bc555c414ce53db40ffe59dcba89be
magnus.coder <kelimbth@gmail.com>
2025-02-20 16:25:16 +0300

    updated code

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 from fastapi import FastAPI
-from db.database import Base, engine
+from db.database import init_db
 from auth import router as auth_router
 from users import router as users_router
 from rides import router as rides_router
 from fastapi.staticfiles import StaticFiles
+from contextlib import asynccontextmanager
+import logging
 
 
-app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-Base.metadata.create_all(bind=engine)
+
+# Define an asynchronous lifespan context manager for the FastAPI app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print('======='*10)     # for decoration
+    logger.info('STARTING UP ... Initializing database.')
+    await init_db()     # initialize database
+    logger.info('DONE ... Database initialized.')
+    print('======='*10)     # for decoration
+    # print('')
+
+    # Yield control back to the FastAPI application — 
+    # this allows the app to run while keeping the lifespan context open
+    yield
+    
+    # this is displayed when the server is shutdown
+    logger.info('SHUTTING DOWN ... Cleaning up resources')
+    logger.info('Wohoo! ... CLEAN UP COMPLETE')
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.mount("/media/dps", StaticFiles(directory="media/dps"), name="uploads")
-
 app.include_router(auth_router, prefix='/api/v1/auth')
 app.include_router(users_router, prefix='/api/v1/users')
 app.include_router(rides_router, prefix='/api/v1')
