f780b48f4a4c0cc1d912ff923fa48899bb925a1c
magnus.coder <kelimbth@gmail.com>
2025-02-24 12:40:49 +0300

    added docstring in router get_profile() and updated router edit_profile()

--- users.py
+++ users.py
@@ -13,37 +13,35 @@ router = APIRouter()
 
 @router.get("/profile", response_model=UserProfile)
 async def get_profile(current_user: UserProfile = Depends(get_current_user), db: AsyncSession = Depends(get_db)):
+    """ This router returns response of the current user """
     return current_user  # FastAPI will automatically convert this to JSON
 
 
-@router.put("/profile/edit")
-async def edit_profile(profile_data: UpdateUserProfile, image: UploadFile = File(...), db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
-    file_location = f"./media/dps/{image.filename}"
+@router.put("/profile/edit", response_model=UpdateUserProfile)
+async def edit_profile(profile_data: str = Form(...), image: UploadFile = File(...), db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_user)):
+    """ Update the current user's profile. """
+    # Convert profile_data string to a dictionary
+    try:
+        profile_data = json.loads(profile_data)
+    except json.JSONDecodeError:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid JSON format for profile_data")
     
-    # Save the uploaded image asynchronously
-    async with aiofiles.open(file_location, "wb") as file_object:
-        while chunk := await image.read(1024):
-            await file_object.write(chunk)
+    if image:
+        file_location = f"./media/dps/{image.filename}"
+        
+        # Save the uploaded image asynchronously
+        async with aiofiles.open(file_location, "wb") as file_object:
+            while chunk := await image.read(1024):
+                await file_object.write(chunk)
     
     # update user profile fields
     current_user.profile_image = file_location
 
     # update user fields dynamically
-    for field, value in profile_data.model_dump().items():
+    for field, value in profile_data.items():
         setattr(current_user, field, value if value is not None else getattr(current_user, field))
 
     await db.commit()
     await db.refresh(current_user)
     
-    return {
-        "id": current_user.id,
-        "username": current_user.username,
-        "email": current_user.email,
-        "profile_image": f"/media/dps/{os.path.basename(file_location)}" if file_location else None,
-         "facebook_handle": current_user.facebook_handle,
-        "instagram_handle": current_user.instagram_handle,
-        "twitter_handle": current_user.twitter_handle,
-        "work_address": current_user.work_address,
-        "home_address": current_user.home_address,
-        "bio": current_user.bio
-    }
+    return current_user
