2c9c2df5fd16afb48718b3001845d314d8b84fe9
magnus.coder <kelimbth@gmail.com>
2025-02-24 11:06:43 +0300

    bug-fix: fixed AttributeError by wrapping TokenBlackList in select() instead of using .select()

--- utils.py
+++ utils.py
@@ -69,7 +69,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/v1/auth/login")
 async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
     # Check if the token is blacklisted
     blacklisted_token = await db.execute(
-        TokenBlacklist.select().where(TokenBlacklist.c.token == token)
+        select(TokenBlacklist).where(TokenBlacklist.c.token == token)
     )
     
     if blacklisted_token.fetchone():
