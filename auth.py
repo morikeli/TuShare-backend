7c96d2b9953fac1bbcb61061047d407b001af6f9
magnus.coder <kelimbth@gmail.com>
2025-02-25 12:53:23 +0300

    bug-fix: update SQL statement since OAuth2PasswordRequestForm expects password and username not email

--- auth.py
+++ auth.py
@@ -27,7 +27,7 @@ Path(UPLOAD_DIR).mkdir(parents=True, exist_ok=True)
 
 @router.post("/login/", status_code=status.HTTP_200_OK, response_model=LoginResponse)
 async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
-    stmt = select(User).where(or_(User.email == form_data.email, User.username == form_data.username))
+    stmt = select(User).where(or_(User.email == form_data.username, User.username == form_data.username))
     result = await db.execute(stmt)
     user = result.scalars().first()
 
