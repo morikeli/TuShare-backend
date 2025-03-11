5f37426a24692c7d3fb3eb3e9e879150874027d6
magnus.coder <kelimbth@gmail.com>
2025-03-11 14:52:00 +0300

    created new db table for messages

--- models.py
+++ models.py
@@ -105,3 +105,24 @@ class TokenBlacklist(Base):
 
     token = Column(String, primary_key=True, index=True)
     created_at = Column(DateTime, nullable=False)
+
+
+class Message(Base):
+    """
+        Database model for storing chat messages between users.
+        Each message is linked to a sender and a receiver (both are users).
+    """
+
+    __tablename__ = "messages"
+
+    id = Column(String, primary_key=True, default=lambda: str(uuid.uuid4().hex), unique=True, index=True)
+    sender_id = Column(String, ForeignKey("users.id"), nullable=False)      # ID of the user sending the message
+    receiver_id = Column(String, ForeignKey("users.id"), nullable=True)    # ID of the user receiving the message, reciever can be null in group chat
+    ride_id = Column(String, ForeignKey("rides.id"), nullable=False)    # Link messages to a ride
+    content = Column(String, nullable=False)    # message text
+    timestamp = Column(DateTime, default=lambda: datetime.now(timezone.utc))    # timestamp when the message was sent
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    # relationships to the User model 
+    sender = relationship("User", foreign_keys=[sender_id])
+    receiver = relationship("User", foreign_keys=[receiver_id], lazy='joined')
