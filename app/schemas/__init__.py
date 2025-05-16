7cb408b47ca9d76f4019f75ebabcf5fca04ba7d0
magnus.coder <kelimbth@gmail.com>
2025-05-16 15:54:05 +0300

    feat: initialize schemas module by importing authentication, messages, rides, and user schemas

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from .auth_schema import LoginRequest, LoginResponse
+from .messages_schema import MessageCreate, MessageResponse
+from .rides_schema import RideCreate, RideResponse
+from .user_schema import CreateUser, CreatedUserResponse, UpdateUserProfile, UserProfile
