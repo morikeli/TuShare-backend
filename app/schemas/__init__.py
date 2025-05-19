f7568da2daee57638af788a43499c4475e64c485
magnus.coder <kelimbth@gmail.com>
2025-05-19 10:43:59 +0300

    feat: expand schema exports in `__init__.py` for enhanced user authentication and booking features

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,4 +1,35 @@
-from .auth_schema import LoginRequest, LoginResponse
+from .auth_schema import ConfirmResetPasswordSchema, LoginRequest, LoginResponse, RequestEmailVerificationSchema, ResetPasswordSchema
+from .booking_schema import BookingResponse, CreateBooking
 from .messages_schema import MessageCreate, MessageResponse
 from .rides_schema import RideCreate, RideResponse
-from .user_schema import CreateUser, CreatedUserResponse, UpdateUserProfile, UserProfile
+from .user_schema import CreateUser, CreatedUserResponse, UpdateUserProfile, UserProfile, UserModel
+
+
+__all__ = [
+    # auth schemas
+    "ConfirmResetPasswordSchema",
+    "LoginRequest",
+    "LoginResponse",
+    "RequestEmailVerificationSchema",
+    "ResetPasswordSchema",
+
+    # booking schemas
+    "BookingResponse",
+    "CreateBooking",
+
+    # messages schemas
+    "MessageCreate",
+    "MessageResponse",
+
+    # rides schemas
+    "RideCreate",
+    "RideResponse",
+
+    # user schemas
+    "CreateUser",
+    "CreatedUserResponse",
+    "UpdateUserProfile",
+    "UserModel",
+    "UserProfile",
+
+]
